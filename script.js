dd6c6984c961dea5abb5118c1185e3bc52f03414
donlonger <lachowski_m@yahoo.com>
2021-08-30 23:07:09 +0200

    added styles, animations

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ let paper = document.getElementById("paper");
 let scissors = document.getElementById("scissors");
 let userScore = document.getElementById("userScore"); 
 let computerScore = document.getElementById("computerScore");
+let userChoice = document.getElementById("userChoice");
 let array = [rock.id, paper.id, scissors.id];
+let elements = document.getElementsByClassName('hide');
+let numberUser = userScore.innerHTML;
+let numberComputer = computerScore.innerHTML;
 
 function computerPlay() {
     let result = array[Math.floor(Math.random() * array.length)];
@@ -22,55 +26,103 @@ function userPlay(id) {
     }
 }
 
+
 function playRound(userPlay, computerPlay) {
-    var elements = document.getElementsByClassName('hide');
-    var numberUser = userScore.innerHTML;
-    var numberComputer = computerScore.innerHTML;
+
+    function scoreAnimation(id) {
+     //   id.setAttribute("class", `animation`);
+
+    }
+
+    function computerWinFormula() {
+        console.log(`You selected ${userPlay}, computer selected ${computerPlay}. Computer Won`);
+        document.getElementsByClassName(`computer${computerPlay}`)[0].style.display='flex';
+        userChoice.setAttribute("id", `${userPlay}`);
+        userChoice.setAttribute("class", `userChoiceStyle ${userPlay}-sizer item`);     
+        numberComputer++;
+        computerScore.innerHTML = numberComputer;
+        computerScore.addEventListener(computerPlay, scoreAnimation(computerScore), false);
+        document.getElementById("computerScore").animate([
+            // keyframes
+            { transform: 'scale(1.5)' },
+            { transform: 'scale(1)' }
+          ], {
+            // timing options
+            duration: 700,
+          });
+          document.getElementById(`comp${computerPlay}`).animate([
+            // keyframes
+            { transform: 'scale(1.5)' },
+            { transform: 'scale(1)' }
+          ], {
+            // timing options
+            duration: 700,
+          });
+    }
+
+    function userWinFormula() {
+        console.log(`You selected ${userPlay}, computer selected ${computerPlay}. You've Won`);
+        document.getElementsByClassName(`computer${computerPlay}`)[0].style.display='flex';
+        userChoice.setAttribute("id", `${userPlay}`);
+        userChoice.setAttribute("class", `userChoiceStyle ${userPlay}-sizer item`); 
+        numberUser++;
+        userScore.innerHTML = numberUser;
+        userScore.addEventListener(playRound, scoreAnimation(userScore), false);
+        document.getElementById("userScore").animate([
+            // keyframes
+            { transform: 'scale(1.5)' },
+            { transform: 'scale(1)' }
+          ], {
+            // timing options
+            duration: 700,
+          });
+          document.getElementById(`comp${computerPlay}`).animate([
+            // keyframes
+            { transform: 'scale(1.5)' },
+            { transform: 'scale(1)' }
+          ], {
+            // timing options
+            duration: 700,
+          });
+
+    }
+
+
 
     for(var i=0; i < elements.length; i++) {  
-        document.getElementsByClassName("hide")[i].style.display = 'none';
+        document.getElementsByClassName("hide")[i].style.display = 'none';    
     }
 
-   // document.getElementsByClassName("hide")[0].style.display = 'none';
     if(userPlay == "rock" && computerPlay =="paper") {
-        console.log(`You selected ${userPlay}, computer selected ${computerPlay}. Computer Won`);
-        document.getElementsByClassName('computerpaper')[0].style.display='flex';
-        numberComputer++;
-        computerScore.innerHTML = numberComputer;
-
+        computerWinFormula();
     }
     else if(userPlay == "rock" && computerPlay =="scissors") {
-        console.log(`You selected ${userPlay}, computer selected ${computerPlay}. You've Won`);
-        document.getElementsByClassName('computerscissors')[0].style.display='flex';
-        numberUser++;
-        userScore.innerHTML = numberUser;
+        userWinFormula();
     }
     else if(userPlay == "paper" && computerPlay =="scissors") {
-        console.log(`You selected ${userPlay}, computer selected ${computerPlay}. Computer Won`);
-        document.getElementsByClassName('computerscissors')[0].style.display='flex';
-        numberComputer++;
-        computerScore.innerHTML = numberComputer;
+        computerWinFormula();
     }
     else if(userPlay == "paper" && computerPlay =="rock") {
-        console.log(`You selected ${userPlay}, computer selected ${computerPlay}. You've Won`);
-        document.getElementsByClassName('computerrock')[0].style.display='flex';
-        numberUser++;
-        userScore.innerHTML = numberUser;
+        userWinFormula();
     }
     else if(userPlay == "scissors" && computerPlay =="paper") {
-        console.log(`You selected ${userPlay}, computer selected ${computerPlay}. You've Won`);
-        document.getElementsByClassName('computerpaper')[0].style.display='flex';
-        numberUser++;
-        userScore.innerHTML = numberUser;
+        userWinFormula();
     }
     else if(userPlay == "scissors" && computerPlay =="rock") {
-        console.log(`You selected ${userPlay}, computer selected ${computerPlay}. Computer Won`);
-        document.getElementsByClassName('computerrock')[0].style.display='flex';
-        numberComputer++;
-        computerScore.innerHTML = numberComputer;
+        computerWinFormula();
     }
     else {
         console.log(`You selected ${userPlay}, computer selected ${computerPlay}. It's a tie`);
         document.getElementsByClassName(`computer${userPlay}`)[0].style.display='flex';
+        userChoice.setAttribute("id", `${userPlay}`);
+        userChoice.setAttribute("class", `userChoiceStyle ${userPlay}-sizer item`);
+        document.getElementById(`comp${computerPlay}`).animate([
+            // keyframes
+            { transform: 'scale(1.5)' },
+            { transform: 'scale(1)' }
+          ], {
+            // timing options
+            duration: 700,
+          });
     }
 }
